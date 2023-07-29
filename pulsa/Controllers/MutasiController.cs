260e146446e7c5a177a922e691195e811009b866
rifki ahmad sururi <rifkisururi27@gmail.com>
2023-07-29 14:35:08 +0700

    add nav header

---
+++ pulsa/Controllers/MutasiController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Pulsa.Web.Controllers
+{
+    [Authorize]
+    public class MutasiController : Controller
+    {
+        private Guid IdLogin { get; set; }
+        public MutasiController(IHttpContextAccessor httpContextAccessor)
+        {
+            var claimsIdentity = httpContextAccessor.HttpContext?.User.Identity as ClaimsIdentity;
+            var idClaim = claimsIdentity.FindFirst("Id");
+
+            if (idClaim != null)
+            {
+                IdLogin = Guid.Parse(idClaim.Value);
+            }
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
